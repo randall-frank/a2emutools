c8be7f178ca2672d5837a102f28368fd1d37d75a
Randall Frank <frogboots.000@gmail.com>
2026-01-24 21:10:53 -0500

    Add import command
    cli 'import' command added.

---
+++ src/a2emutools/cmd_import.py
@@ -0,0 +1,12 @@
+import logging
+from typing import List
+
+from . import container_formats, filesystem  # noqa: F401
+
+_ = logging.getLogger("a2emutools")
+
+
+def cmd_import(
+    cont_name: str, prefix: str, names: List[str], tokenize: bool = False, force_type: str = ""
+):
+    return
